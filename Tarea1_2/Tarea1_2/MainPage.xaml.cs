0d7c2f59f197141c3b26e04782201530afbc0870
g5llo <gallorubi005@gmail.com>
2023-02-21 18:46:07 -0600

    version1

---
+++ Tarea1_2/Tarea1_2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void datos()
+        {
+            //var nombre = string.Parse(nom.Text);
+            //await Navigation.PushAsync(new Views.PageTwo(suma));
+        }
+
+    }
+}
---
+++ Tarea1_2/Tarea1_2/Views/Page2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1_2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        public Page2(String nombre, string apellido, string edad, string correo)
+        {
+            InitializeComponent();
+
+            lbNombre.Text = nombre;
+            lbApellido.Text = apellido;
+            lbEdad.Text = edad;
+            lbCorreo.Text = correo;
+        }
+    }
+}
